dfca8eb30e1930ebcab70a91d2a888600c6f46b7
amirbettermode <amirhossein@bettermode.com>
2025-10-22 19:14:36 +0400

    Update background image positioning to fixed and increase width to 180vw for better overflow effect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,11 +9,11 @@ export default function Home() {
   return (
     <div className="min-h-screen relative overflow-hidden" style={{ backgroundColor: '#2F01B0' }}>
       {/* Background Image - Larger than screen, centered, overflows */}
-      <div className="absolute -top-20 left-1/2 -translate-x-1/2 pointer-events-none">
+      <div className="fixed -top-20 left-1/2 -translate-x-1/2 pointer-events-none">
         <img 
           src="/bg.png" 
           alt="background"
-          className="w-[150vw] h-auto"
+          className="w-[180vw] h-auto"
         />
       </div>
 
