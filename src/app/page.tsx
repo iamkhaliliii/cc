137efa35cc2f21a1ca6c13388577aa94c772507c
amirbettermode <amirhossein@bettermode.com>
2025-10-22 19:08:20 +0400

    Fix background size - use height-based sizing to overflow horizontally

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,12 +8,12 @@ export default function Home() {
 
   return (
     <div className="min-h-screen relative overflow-hidden" style={{ backgroundColor: '#2F01B0' }}>
-      {/* Background Image - 2x screen size, overflows left and right */}
-      <div className="absolute -top-20 left-1/2 -translate-x-1/2 pointer-events-none w-screen">
+      {/* Background Image - Larger than screen, centered, overflows */}
+      <div className="absolute -top-20 left-1/2 -translate-x-1/2 pointer-events-none">
         <img 
           src="/bg.png" 
           alt="background"
-          className="w-[200vw] h-auto -ml-[50vw]"
+          className="h-[120vh] w-auto"
         />
       </div>
 
